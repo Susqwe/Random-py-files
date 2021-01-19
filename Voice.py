906db7fa312491ca8e0c443a5e36c4d4642ff1ac
Susqwe <44109570+Susqwe@users.noreply.github.com>
2021-01-19 15:00:19 +0600

    Add files via upload

---
+++ Voice.py
@@ -0,0 +1,32 @@
+import speech_recognition as sr
+
+
+def command():
+	r = sr.Recognizer()
+
+	with sr.Microphone() as source:
+		print("Говорите")
+		r.pause_threshold = 1
+		r.adjust_for_ambient_noise(source, duration=1)
+		audio = r.listen(source) 
+
+	try:
+		task = r.recognize_google(audio, language="ru-RU").lower()
+		print("Вы сказали " + task)
+	except:
+		task = command()
+	return task
+
+
+def makeSomething(task):
+	print(task)
+	if "сайт" in task:
+		print("hello")
+	elif "питон" in task:
+		print("Python <3")
+	else:
+		print("Иех, скукота")
+
+
+while True:
+	makeSomething(command())
